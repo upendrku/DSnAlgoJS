f9b430815318fc4d00effd585be459f1429a34b5
upend1310 <shawkumarupendra@outlook.com>
2023-01-20 17:23:45 -0500

    Added as well as edited for a better solution

--- Recursion/isPalindrome.js
+++ Recursion/isPalindrome.js
@@ -1,11 +1,18 @@
+// function isPalindrome(str){
+//     let strArr = str.length > 0 ? str.split('') : []
+//     function reverse(str){
+//         let strArr = str.length > 0 ? str.split('') : []
+//         return strArr.length === 0 ? '' : strArr.pop() + reverse(strArr.join(''))
+//     }
+//     const reversedStr = strArr.length === 0 ? '' : strArr.pop() + reverse(strArr.join(''))
+//     return reversedStr === str
+// }
+
 function isPalindrome(str){
-    let strArr = str.length > 0 ? str.split('') : []
-    function reverse(str){
-        let strArr = str.length > 0 ? str.split('') : []
-        return strArr.length === 0 ? '' : strArr.pop() + reverse(strArr.join(''))
-    }
-    const reversedStr = strArr.length === 0 ? '' : strArr.pop() + reverse(strArr.join(''))
-    return reversedStr === str
+    if(str.length === 1) return true;
+    if(str.length === 2) return str[0] === str[1];
+    if(str[0] === str.slice(-1)) return isPalindrome(str.slice(1,-1))
+    return false;
 }
 
 isPalindrome('awesome') // false
