1245877250e65f6d2c703bdafa3f2514eb208651
upend1310 <shawkumarupendra@outlook.com>
2023-01-23 21:31:16 -0500

    Added another way to implement

--- Data Structures/Singly Linked List/simplestImplementation.js
+++ Data Structures/Singly Linked List/simplestImplementation.js
@@ -2,11 +2,25 @@
 // you can use this
 
 class Node {
-    constructor (value, next) {
+    constructor(value, next) {
         this.value = value;
         this.next = next ? next : null;
     }
 }
+
+// const initializeLinkedListFromArray = array => {
+//     if (array. length === 0) {
+//         return [];
+//     }
+//     let head = new Node(array[0]);
+//     let current = head;
+//     for (let i = 1; i < array.length; i++) {
+//         let newNode = new Node(array[i]);
+//         current.next = newNode;
+//         current = current.next;
+//     }
+//     return head;
+// }
     
 const initializeLinkedListFromArray = array => {
     return array.reverse().reduce((acc, curr) => new Node(curr, acc), null)
