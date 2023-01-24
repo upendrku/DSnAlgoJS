b2e8edcf71b18584661c37d5fcbd5a9d4f84e457
upend1310 <shawkumarupendra@outlook.com>
2023-01-23 21:30:01 -0500

    Singly linked list for coding interviews

---
+++ Data Structures/Singly Linked List/simplestImplementation.js
@@ -0,0 +1,15 @@
+// In cases where you are just given an array and want to convert it to singly linked list and use it for further operations
+// you can use this
+
+class Node {
+    constructor (value, next) {
+        this.value = value;
+        this.next = next ? next : null;
+    }
+}
+    
+const initializeLinkedListFromArray = array => {
+    return array.reverse().reduce((acc, curr) => new Node(curr, acc), null)
+}
+
+console.log(initializeLinkedListFromArray([99, 56, 1000, 356, 566, 999, 1, 0]))
