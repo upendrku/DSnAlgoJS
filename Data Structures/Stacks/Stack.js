3485d96e7c9873811650c1a8b81155188201dbab
upend1310 <shawkumarupendra@outlook.com>
2023-01-27 18:45:10 -0500

    Stack implementation

---
+++ Data Structures/Stacks/Stack.js
@@ -0,0 +1,36 @@
+class Node {
+    constructor(value){
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class Stack {
+    constructor(){
+        this.first = null;
+        this.last = null;
+        this.length = 0;
+    }
+    push(val){
+        var newNode = new Node(val);
+        if(!this.first){
+            this.first = newNode;
+            this.last = newNode;
+        } else {
+            var temp = this.first;
+            this.first = newNode;
+            this.first.next = temp;
+        }
+        return ++this.length;
+    }
+    pop(){
+        if(!this.first) return null;
+        var temp = this.first;
+        if(this.first === this.last){
+            this.last = null;
+        }
+        this.first = this.first.next;
+        this.length--;
+        return temp.value;
+    }
+}
