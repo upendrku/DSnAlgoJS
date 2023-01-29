cea54f18878be13770786007e11ecba66dd97c39
upend1310 <shawkumarupendra@outlook.com>
2023-01-29 01:37:27 -0500

    MaxBinaryHeap Implementation for insert

---
+++ Data Structures/Tree/Binary Heaps/Max_Binary_Heap_Insert.js
@@ -0,0 +1,36 @@
+class MaxBinaryHeap {
+    constructor(){
+        this.values = [];
+    }
+    insert(element){
+        this.values.push(element);
+        this.bubbleUp();
+    }
+    bubbleUp(){
+        let idx = this.values.length - 1;
+        const element = this.values[idx];
+        while(idx > 0){
+            let parentIdx = Math.floor((idx - 1)/2);
+            let parent = this.values[parentIdx];
+            if(element <= parent) break;
+            this.values[parentIdx] = element;
+            this.values[idx] = parent;
+            idx = parentIdx;
+        }
+    }
+}
+
+// to find child of a node we can use 2n + 1 and 2n + 2
+// to find parent similarly we use floor((n - 1) / 2)
+
+let heap = new MaxBinaryHeap();
+heap.insert(41);
+heap.insert(39);
+heap.insert(33);
+heap.insert(18);
+heap.insert(27);
+heap.insert(12);
+heap.insert(55);
+
+
+
