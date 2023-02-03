36c9d73530aefc2f5457d08de604317c48d4a214
upend1310 <shawkumarupendra@outlook.com>
2023-02-03 13:50:38 -0500

    Implemented Dijkstra algo

---
+++ Data Structures/Graphs/Weighted Graph/simplePriorityQueue.js
@@ -0,0 +1,16 @@
+// This is simpleset imlementation not the best. Best imlementation is on Data Structures/Tree/Priority Queue/Priority_Queue.js
+class PriorityQueue {
+  constructor(){
+    this.values = [];
+  }
+  enqueue(val, priority) {
+    this.values.push({val, priority});
+    this.sort();
+  };
+  dequeue() {
+    return this.values.shift();
+  };
+  sort() {
+    this.values.sort((a, b) => a.priority - b.priority);
+  };
+}
